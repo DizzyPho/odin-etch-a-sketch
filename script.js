ee50b4e2c8f1b5af908e56395420dce2b5983a13
dizzypho <184835166+DizzyPho@users.noreply.github.com>
2025-05-13 15:42:24 +0200

    Add function to add squares to the grid, amount depending on input

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function createGridSquare() {
     square.classList.toggle("gridSquare");
     return square;
 }
+
+function fillGrid(size) {
+    const squaresAmount = size ** 2;
+    const grid = document.querySelector("#container");
+    for (let i = 0; i < squaresAmount; i++) {
+        const square = createGridSquare();
+        grid.appendChild(square);
+    }
+}
+
+fillGrid(16);
