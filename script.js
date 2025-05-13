85fc447bb0d6c9b4184701fbb4f9197070b2bc09
dizzypho <184835166+DizzyPho@users.noreply.github.com>
2025-05-13 16:18:23 +0200

    Style squares in the grid container to display evenly as a grid

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ function createGridSquare() {
 
 function fillGrid(size) {
     const squaresAmount = size ** 2;
+    const squareSize = 100/size;
     const grid = document.querySelector("#container");
+ 
     for (let i = 0; i < squaresAmount; i++) {
         const square = createGridSquare();
+        square.style.flex = `1 1 ${squareSize}%`;
+        square.style.height = `${squareSize}%`  
         grid.appendChild(square);
     }
 }
 
-fillGrid(16);
+fillGrid(10);
